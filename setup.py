f91f19e229bb2882be657192ef257f8b789fbcec
Shenal Harshana Fernando Elekuttige <45353233+shenaal1992@users.noreply.github.com>
2024-07-04 01:02:21 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hello_world_package',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple Python package that prints Hello, World!',
+    author='Shenal',
+    
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
